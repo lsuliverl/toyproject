6ff4e26666cab19ba7d6a2f8d61bc0cde9a3e775
lsuliverl <sh000803@naver.com>
2024-03-06 15:24:51 +0900

    localStorage에 유저 정보가 없다면 로그인 화면 보여주고 있다면 인사말 표시 구현

--- javascript/toDoList/app.js
+++ javascript/toDoList/app.js
@@ -3,13 +3,26 @@ const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
 
 const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
 
 function onLoginSubmit(event) {
     event.preventDefault();
-    const username = loginInput.value;
     loginForm.classList.add(HIDDEN_CLASSNAME);
-    greeting.innerText = `안녕하세요 ${username}`;
-    greeting.classList.remove(HIDDEN_CLASSNAME)
+    const username = loginInput.value;
+    localStorage.setItem(USERNAME_KEY, username);
+    paintGreetings(username);
 }
 
-loginForm.addEventListener("submit", onLoginSubmit)
+function paintGreetings(username) {
+    greeting.innerText = `안녕하세요 ${username}님`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if (savedUsername === null) {
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+    paintGreetings(savedUsername);
+}
