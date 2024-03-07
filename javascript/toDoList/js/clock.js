f23acf27293efdcecb3e51b4107aefb01ff09fcf
lsuliverl <sh000803@naver.com>
2024-03-07 14:41:37 +0900

    clock.js 주석 추가

--- javascript/toDoList/js/clock.js
+++ javascript/toDoList/js/clock.js
@@ -2,11 +2,11 @@ const clock = document.querySelector("h2#clock");
 
 function getClock() {
     const date = new Date();
-    const hours = String(date.getHours()).padStart(2,"0");
+    const hours = String(date.getHours()).padStart(2,"0");      // 2글자가 아닐때 앞에 0 추가 ex) 1 -> 01, 23 -> 23
     const minutes = String(date.getMinutes()).padStart(2,"0");
     const seconds = String(date.getSeconds()).padStart(2,"0");
     clock.innerText = `${hours}:${minutes}:${seconds}`;
 }
 
 getClock();
-setInterval(getClock, 1000);
+setInterval(getClock, 1000);    // getClock함수 1초마다 재실행
