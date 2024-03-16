733213c0e227810fe5cc0fdfaf517bfc79cf30de
lsuliverl <sh000803@naver.com>
2024-03-16 15:47:11 +0900

    위치에 따른 날씨 구현 및 api키 숨기기

---
+++ javascript/toDoList/js/weather.js
@@ -0,0 +1,23 @@
+import config from "./config.js";
+
+const { API_KEY } = config
+
+function onGeoOk(position) {
+    const lat = position.coords.latitude;
+    const lon = position.coords.longitude;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+    fetch(url)
+        .then((response) => response.json())
+        .then((data) => {
+            const weather = document.querySelector("#weather span:first-child");
+            const city = document.querySelector("#weather span:last-child");
+            city.innerText = data.name;
+            weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+        });
+}
+
+function onGeoError() {
+    alert("위치 정보를 찾을 수 없어 날씨를 제공 할 수 없습니다.") 
+}
+
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
