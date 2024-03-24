4552e35deac7355690e93f1e9a9e2bd28fc1d5bc
lsuliverl <sh000803@naver.com>
2024-03-24 13:58:24 +0900

    백그라운드 이미지 아이디 추가, 날씨 아이콘 구현

--- javascript/toDoList/js/background.js
+++ javascript/toDoList/js/background.js
@@ -15,6 +15,7 @@ const chosenImage = images[Math.floor(Math.random() * images.length)];
 
 const bgImage = document.createElement("img");      // img Element 생성
 
+bgImage.id = `bgImg`;                   // 아이디 추가
 bgImage.src = `img/${chosenImage}`;     // img Element에 경로 추가, ${chosenImage}은 랜덤 이미지
 
 document.body.appendChild(bgImage);     // body에 bgImage에서 만든 img Element 추가
--- javascript/toDoList/js/weather.js
+++ javascript/toDoList/js/weather.js
@@ -8,11 +8,16 @@ function onGeoOk(position) {
     const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;          //위도와 경도 그리고 API 키를 사용하여 OpenWeatherMap API에 요청할 URL을 생성
     fetch(url)                                                                                  // 주어진 URL로 GET 요청을 보내어 데이터를 가져온다
         .then((response) => response.json())                                                    // 가져온 응답(response)을 JSON 형식으로 변환
-        .then((data) => {                                                                       
+        .then((data) => {
+            const div = document.querySelector("#weather");                                                                      
             const weather = document.querySelector("#weather span:first-child");
             const city = document.querySelector("#weather span:last-child");
-            city.innerText = data.name;                                                         // 현재 위치의 도시 이름 text 출력
-            weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;                  // 날씨와 온도 text 출력
+            city.innerText = `${data.name}`;                                                         // 현재 위치의 도시 이름 text 출력
+            weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;                      // 날씨와 온도 text 출력
+            const img = document.createElement("img");                                              // img Element 생성
+            div.appendChild(img);                                                                   // div에 img 자식 추가
+            img.src = `https://openweathermap.org/img/wn/${data.weather[0].icon}.png`               // 이미지 경로 설정
+            img.alt = `${data.weather[0].description}`                                              // 해당 이미지를 대체할 텍스트를 명시 설정
         });
 }
 
