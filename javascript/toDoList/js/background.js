ffe742955254037121938b15c879337fd5863c29
lsuliverl <sh000803@naver.com>
2024-03-24 16:40:37 +0900

    백그라운드 이미지 축소(글씨가 잘 안 보이는것들)

--- javascript/toDoList/js/background.js
+++ javascript/toDoList/js/background.js
@@ -4,11 +4,6 @@ const images = [
     "2.jpg",
     "3.jpg",
     "4.jpg",
-    "5.jpg",
-    "6.jpg",
-    "7.jpg",
-    "8.jpg",
-    "9.jpg",
 ];
 
 const chosenImage = images[Math.floor(Math.random() * images.length)];
